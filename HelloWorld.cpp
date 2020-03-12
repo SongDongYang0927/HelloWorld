14d5b1529a5e7711a47bacb9a9e74442798f2632
songdongyang <913680882@qq.com>
2020-03-12 22:51:24 +0800

    添加Add函数

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,8 +2,16 @@
 
 using namespace std;
 
+int Add(int a, int b)
+{
+	return a + b;
+}
+
 int main()
 {
+	int a = 10;
+	int b = 20;
+	cout << a << " + " << b << " = " << Add(a, b) << endl;
 	cout << "Hello World!" << endl;
 	return 0;
 }
