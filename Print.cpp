98d983847a8f201ce3d4bf99eb23a40d4c4f0765
songdongyang <songdongyang0927@aliyun.com>
2020-03-13 11:35:14 +0800

    新增加文件Print.cpp

---
+++ Print.cpp
@@ -0,0 +1,12 @@
+#ifndef __PRINT_CPP__
+#define __PRINT_CPP__
+
+#include <iostream>
+
+using namespace std;
+
+void Print(int a, int b){
+	cout << "a = " << a << ", b = ", << b << endl;
+}
+
+#endif
